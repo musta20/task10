ae8071b3efa2fce7fe0db075ee0df9dc7a46336f
mustafa <must.g20@gmail.com>
2022-06-13 04:30:47 +0300

    add env

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 
 const app = express();
 
+const path = require('path');
+
 const UserRoute = require("./Controllers/UserController")
 
 const PORT = process.env.PORT || 4000;
@@ -17,6 +19,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use(UserRoute);
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
   
 app.listen(PORT,()=>{
